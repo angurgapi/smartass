b2c023b309eeeb2024ed2cbe3637a51099a39b6a
angurgapi <sophiekovalsky@gmail.com>
2022-12-07 01:02:16 +0400

    basic grid

---
+++ pages/riddle.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="page">
+        <div class="grid">
+            <div class="imgtile" v-for="(img, index) in images" :key="index" :style="`background-image: url('/art/${img}.webp')`">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        images: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
+        ]
+    })
+}
+</script>
+
+<style lang="scss">
+.grid {
+    display: grid;
+    grid-gap: 18px;
+    grid-template-columns: repeat(4, 1fr);
+}
+.imgtile {
+    height: 200px;
+    width: 300px;
+    background-size: cover;
+}
+</style>
